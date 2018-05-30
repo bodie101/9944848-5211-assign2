f0cf87284db155039a23594e1628a5cf24bda0c4
bodie101 <keysbw@g.toiohomai.ac.nz>
2018-05-30 20:09:56 +1200

    Q2-Made it so each item on the list gets printed on a new line

--- question02/Program.cs
+++ question02/Program.cs
@@ -8,7 +8,7 @@ namespace question02
 
         static void Main(string[] args)
         {
-            string[] fruits = new string[4] {"Apple", "Banana", "Coconut", "Mandarin"};
+            string[] fruits = new string[4] {"Apple \n", "Banana\n", "Coconut\n", "Mandarin\n"};
 
             foreach(var fruit in fruits) {
                 Console.Write(fruit);
