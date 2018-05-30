8e9068116f968120893fef2a984864721bc3dd59
bodie101 <keysbw@g.toiohomai.ac.nz>
2018-05-30 12:06:14 +1200

    Q1-Put curly braces in for the fname and lname strings line 9

--- question01/Program.cs
+++ question01/Program.cs
@@ -6,7 +6,7 @@ namespace question01
     {
         static string Greeting(string fname, string lname) {
 
-            return $"Hello (fname) (lname) enjoy this assignemnt :-)";
+            return $"Hello {fname} {lname} enjoy this assignemnt :-)";
         }
         static void Main(string[] args)
         {
