858a4b7478b8d1df9c7262393d4224673c1d11be
bodie101 <keysbw@g.toiohomai.ac.nz>
2018-05-31 11:44:34 +1200

    Q3-Added Stopwatch

--- question03/Program.cs
+++ question03/Program.cs
@@ -8,12 +8,19 @@ namespace question03
     {
         static void Main(string[] args)
         {
+            Stopwatch sw = new Stopwatch(); 
+
+            sw.Start();
+
             string[] fruitsArray = new string[] {"Banana", "Grapefruit", "Tomato", "Oranges"};
 
             //List<string> fruitsArray = new List<string> {"Banana", "Grapefruit", "Tomato", "Oranges"};
 
             foreach(var fruit in fruitsArray) {
                 Console.WriteLine(fruit);
+
+            sw.Stop();
+            Console.WriteLine(sw.Elapsed);     
             }
         }
     }
