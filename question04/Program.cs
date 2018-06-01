543d1008d67e8708f867b6cfb4903c8958a4a9f9
bodie101 <keysbw@g.toiohomai.ac.nz>
2018-06-01 17:14:24 +1200

    Q4-Put in tryParse to turn string into a int

---
+++ question04/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace question04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Print out the reverse timestables (highest number first) for:");
+            var number = Console.ReadLine();
+
+            var number1 = 0; 
+            var isNumber = int.TryParse(number, out number1); 
+
+            for(var i = 0; i < 12; i++) {
+                var a = i + 1;
+                Console.WriteLine($"{a} x {number} = {a * number1}");
+            }
+        }
+    }
+}
